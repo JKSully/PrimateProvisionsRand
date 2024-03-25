d089e94a7d9ca3a1753e59cac862baed6cef3a37
jksully <jksully@umich.edu>
2024-03-25 14:31:41 -0400

    Increased percision from minutes to seconds

--- src/random_motor_controller.cpp
+++ src/random_motor_controller.cpp
@@ -12,7 +12,7 @@ RandomMotorController::RandomMotorController() : RandomMotorController(1) {}
 RandomMotorController::RandomMotorController(int header) : MotorController(header), random_device_(), mt19937_(random_device_()) {
     std::int32_t num_distributions = 10;
     static auto constexpr const time_span = 1.0h;
-    auto time_segments = std::chrono::duration_cast<std::chrono::minutes>(time_span) / num_distributions;
+    auto time_segments = std::chrono::duration_cast<std::chrono::seconds>(time_span) / num_distributions;
 
     std::int32_t const rhs = static_cast<std::int32_t>(time_segments.count());
     std::uniform_int_distribution<std::int32_t> distribution(1, rhs);
@@ -26,28 +26,24 @@ RandomMotorController::RandomMotorController(int header) : MotorController(heade
         if (val == 1) {
             disperse();
             auto now = std::chrono::high_resolution_clock::now();
-            auto time_elapsed = std::chrono::duration_cast<std::chrono::minutes>(now - iter_start_time);
+            auto time_elapsed = std::chrono::duration_cast<std::chrono::seconds>(now - iter_start_time);
             // Debugging
-            auto cur_time = std::chrono::high_resolution_clock::now();
-            auto time_between = std::chrono::duration_cast<std::chrono::minutes>(cur_time - prev_time);
-            std::cout << "Time elapsed: " << time_between.count() << "m" << std::endl;
-            prev_time = cur_time;
+            std::cout << "Time elapsed: " << time_elapsed.count() << "s" << std::endl;
             // end debug
             std::this_thread::sleep_for(time_segments - time_elapsed);
             iter_start_time = std::chrono::high_resolution_clock::now();
             --num_distributions;
-        } else if (std::chrono::duration_cast<std::chrono::minutes>(std::chrono::high_resolution_clock::now() - iter_start_time) >= time_segments) {
+        } else if (std::chrono::duration_cast<std::chrono::seconds>(std::chrono::high_resolution_clock::now() - iter_start_time) >= time_segments) {
             disperse();
+            auto now = std::chrono::high_resolution_clock::now();
+            auto time_elapsed = std::chrono::duration_cast<std::chrono::seconds>(now - iter_start_time);
             iter_start_time = std::chrono::high_resolution_clock::now();
             --num_distributions;
             // Debugging
-            auto cur_time = std::chrono::high_resolution_clock::now();
-            auto time_between = std::chrono::duration_cast<std::chrono::minutes>(cur_time - prev_time);
-            std::cout << "Time elapsed: " << time_between.count() << "m" << std::endl;
-            prev_time = cur_time;
+            std::cout << "Time elapsed: " << time_elapsed.count() << "s" << std::endl;
             // end debug
         } else {
-            std::this_thread::sleep_for(1min);
+            std::this_thread::sleep_for(1s);
         }
     }
 }
